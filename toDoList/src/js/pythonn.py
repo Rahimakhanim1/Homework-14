c523b19715f5fe47d2ca754b5174d4ac07af84e1
rahimakhanim <rehimeagazade@gmail.com>
2023-09-28 23:12:01 +0400

    djndnjdd

---
+++ toDoList/src/js/pythonn.py
@@ -0,0 +1,8 @@
+reversedName = "gnimmargorP nohtyP"
+name= ""
+for i in range(len(reversedName)-1,-1,-1):
+    name+=reversedName[i]
+print(name)
+
+# Python Programming
+ 
--- toDoList/src/js/script.js
+++ toDoList/src/js/script.js
@@ -1,43 +1,80 @@
-// window.addEventListener("DOMContentLoaded",function(){
-//     let btn = document.getElementById("btn");
-//     let input = document.getElementById("userInput");
-//     console.log('hello')
-
-
-
-//     btn.addEventListener("click",function(){
-//         console.log(input.value)
-//         if(input.value.length>0){
-//             console.log(input.value.length)
-//         }
-//     }
-
-//     );
-// }
-// );
-
 let btn = document.getElementById("btn");
 let input = document.getElementById("userInput");
 let toDoList = document.getElementById("to-do-list");
-let colorList = ["rgb(35, 225, 35)","rgb(222, 198, 45)","pink",]
+let checked = document.querySelector("#checked");
+let spanTags ;
 
-btn.addEventListener("click",function(){
-    let index = 0;
-    console.log(input.value)
-    if(input.value.length>0){
+
+
+let colorList = ["rgb(35, 225, 35)","rgb(222, 198, 45)","pink",]
+let index = 0,listIndex = 1,spanIndex,bool = true;
+function addItem(){
+    
+    if(input.value.length>0 && listIndex<12){
+        spanIndex = "span-"+listIndex;
+        listIndex++;
         let listItem = document.createElement("div");
         listItem.classList.add("list-item");
         let span = document.createElement("span");
         span.style.borderColor = colorList[index];
         index++;
-        let spanI = document.createElement("i");
-        span.appendChild(spanI);
-        toDoList.appendChild(span);
+        if(index>colorList.length-1){
+            index=0
+        }
+        // let spanI = document.createElement("i");
+        // span.appendChild(spanI);
+        span.setAttribute("id","checked-"+listIndex);
+
+        span.classList.add("span");
+        span.addEventListener('click',function(){
+            if(!this.firstElementChild){
+                this.innerHTML = ' <i id="ret" class="fas fa-check"></i>';
+            }else{
+                this.firstElementChild.remove("i")
+            }
+        });
+        // span.setAttribute("onclick","check()");
+        listItem.appendChild(span);
         let listBorder = document.createElement("div");
         listBorder.classList.add("list-border");
-        let liElement = document.createElement("li");
-        li.innerText = input.value;
-        listBorder.appendChild(li);
-        toDoList.appendChild(listBorder);
+        let listElement = document.createElement("li");
+        listElement.innerText = input.value;
+        listBorder.appendChild(listElement);
+        listItem.appendChild(listBorder);
+        toDoList.appendChild(listItem);
+        input.value ="";
+        spanTags = document.querySelectorAll(".span");
+       
 
-}});
+    }
+   
+};
+
+// for(let i = 0; i < spanTags.length; i++){
+//     spanTags[i].addEventListener('click',function(){
+//         if(!this.firstElementChild){
+//             this.innerHTML = ' <i id="ret" class="fas fa-check"></i>';
+//         }else{
+//             this.firstElementChild.remove("i")
+//         }
+//     });
+// }
+
+// checked.addEventListener('click',function(){
+// console.log("check olundu")        
+//     }
+// );
+
+// function check(){
+
+//     let spanItem = this.event.srcElement;
+//     let iconTag = spanItem.querySelector("i");
+//     console.log(spanItem)
+//     console.log(iconTag)
+//     if(!iconTag){
+//         spanItem.innerHTML='<i class="fas fa-check"></i>';
+//     }else{
+//         spanItem.remove("i");
+//     }
+   
+// }
