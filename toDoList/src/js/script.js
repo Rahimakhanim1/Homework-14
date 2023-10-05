1ff432b1b7a1cff896d3d247deaac94e22797ea6
rahimakhanim <rehimeagazade@gmail.com>
2023-10-05 19:29:39 +0400

    neww

--- toDoList/src/js/script.js
+++ toDoList/src/js/script.js
@@ -13,9 +13,10 @@ function addItem(){
         <div class="list-border">   
                 <svg onclick = "remove(this)" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>                      
             <li>${input.value}</li>
-        </div>                       
-    </div>`
+                 </div>                       
+         </div>`
         index++;
+
         if(index>colorList.length-1){
             index=0
         }
@@ -25,10 +26,12 @@ function addItem(){
 function check(span){
     if(!span.firstElementChild){
         span.innerHTML = '<i class="fas fa-check"></i>';
-    }else{
+    }
+    else{
         span.firstElementChild.remove("i")
             }
 };
 function remove(item){
-    item.parentElement.parentElement.style.display = 'none';  
+    toDoList.removeChild(item.parentNode.parentNode)
+   
 }
